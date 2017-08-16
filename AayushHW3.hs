e90f8d6da607eb8de0ff811a9c507e55a00c4775
Aayush Rajasekaran <arajasek94@gmail.com>
2017-08-16 12:35:39 -0400

    HW3 complete

--- AayushHW3.hs
+++ AayushHW3.hs
@@ -19,3 +19,24 @@ localMaxima (a:b:c:l)
  | (b > a) && (b > c) = (b : localMaxima (b:c:l))
  | otherwise = localMaxima(b:c:l)
 localMaxima _ = []
+
+count   :: Eq a => a -> [a] -> Int
+count x =  length . filter (==x)
+
+highestFrequency :: [Integer] -> Integer -> Int -> Int
+highestFrequency l m currbes
+ | (m > 9) = currbes
+ | (count m l) > currbes = (highestFrequency l (m+1) (count m l))
+ | otherwise = (highestFrequency l (m+1) currbes)
+
+
+processList :: [Integer] -> Integer -> Int -> String
+processList l m n
+ | (n <= 0) = []
+ | (m > 9) = ('\n':(processList l 0 (n-1)))
+ | (count m l) >= n = ('*' : (processList l (m+1) n))
+ | otherwise = (' ' : (processList l (m+1) n))
+
+
+histogram :: [Integer] -> String
+histogram l = (processList l 0 (highestFrequency l 0 0)) ++ "==========\n0123456789\n"
