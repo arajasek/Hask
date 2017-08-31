87629de80d6b003aa9d65c8475ff9f240932bbf8
Aayush Rajasekaran <arajasek94@gmail.com>
2017-08-31 16:30:52 -0400

    HW7 almost done

--- HW7/HW7.hs
+++ HW7/HW7.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
 import Data.Monoid
 import Sized
+import Scrabble
+import Buffer
+import Editor
 
 data JoinList m a = Empty
  | Single m a
@@ -36,6 +40,64 @@ dropJ _ Empty = Empty
 dropJ i l 
  | i <= 0 = l
  | i >= intTag l = Empty
-dropJ i l@(Append m l1 l2)
- | i <= intTag l1 = Append (((intTag l) - i) <> mempty) (dropJ i l1) l2
- | otherwise = Append (((intTag l) - i) <> mempty) Empty (dropJ (i - intTag l1) l2)
+dropJ i (Append _ l1 l2)
+ | i < intTag l1 = (dropJ i l1) +++ l2
+ | otherwise = dropJ (i - intTag l1) l2
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ _ Empty = Empty
+takeJ i l 
+ | i <= 0 = Empty
+ | i >= intTag l = l
+takeJ i (Append _ l1 l2)
+ | i <= intTag l1 = takeJ i l1
+ | otherwise = l1 +++ takeJ (i - intTag l1) l2
+
+--3
+
+scoreLine :: String -> JoinList Score String
+scoreLine l = Single (scoreString l) l
+
+-- 4
+
+instance Buffer (JoinList (Score, Size) String) where
+ toString Empty = ""
+ toString (Single _ a) = a
+ toString (Append _ l r) = (toString l) ++ (toString r)
+
+ fromString = foldr (+++) Empty . 
+                map (\l -> Single ((scoreString l), (Size 1)) l) . lines
+
+ line n l
+  | (n >= intTag l) || (n < 0) = Nothing
+  | (n == 0) = Just (toString l)
+
+ line n (Append _ l r)
+  | (n < intTag l) = line n l
+  | otherwise = line (n - intTag l) r
+
+ replaceLine n ln buf
+  | (n < 0) || (n > intTag buf) = buf
+  | (n == 0) = fromString ln
+
+ replaceLine n ln (Append m l r)
+  | (n < intTag l) = (Append m (replaceLine n ln l) r)
+  | otherwise = (Append m l (replaceLine (n - intTag l) ln r))
+
+ numLines = intTag
+
+ value Empty = 0
+ value (Single ((Score i), _) _) = i
+ value (Append ((Score i), _) _ _) = i
+
+main = runEditor editor $ fromString unlines
+         [ "This buffer is for notes you don't want to save, and for"
+         , "evaluation of steam valve coefficients."
+         , "To load a different file, type the character L followed"
+         , "by the name of the file."
+         ]
+
+
+
+
+
---
+++ HW7/Scrabble.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances #-}
+module Scrabble where
+
+import Data.Monoid
+import Data.Char
+
+newtype Score = Score Int
+  deriving (Eq, Ord, Show, Num)
+
+instance Monoid Score where
+  mempty  = Score 0
+  mappend = (+)
+
+score :: Char -> Score
+score inp
+ | elem c "aeioulnstr" = Score 1
+ | elem c "dg" = Score 2
+ | elem c "bcmp" = Score 3
+ | elem c "fhvwy" = Score 4
+ | c == 'k' = Score 5
+ | elem c "jx" = Score 8
+ | elem c "qz" = Score 10
+ | otherwise = Score 0
+ where c = toLower inp
+
+scoreString :: String -> Score
+scoreString = foldr mappend mempty . map score
