7215b053fe2c4837a06e1df523e882f87ff23242
Rein Otsason <reinotsason@gmail.com>
2017-09-02 18:49:42 -0400

    Homework 8 upto Exercise 4

---
+++ HW8/Party.hs
@@ -0,0 +1,46 @@
+module Party where
+import Employee
+import Data.Tree
+import Data.Monoid
+
+glCons :: Employee -> GuestList -> GuestList
+glCons emp (GL emps fun) = GL (emp:emps) (fun + empFun emp)
+
+instance Monoid GuestList where
+  mempty = GL [] 0
+  mappend (GL xs nx) (GL ys ny) = GL (xs ++ ys) (nx + ny)
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun a b
+  | a > b     = a
+  | otherwise = b
+
+treeFold :: (a -> b -> b) -> b -> Tree a -> b
+treeFold f init tree = case subForest tree of
+                         [] -> newInit
+			 _  -> foldr f' newInit (subForest tree)
+			 where newInit = f (rootLabel tree) init 
+			       f' = \t i -> treeFold f i t
+
+treeFold1 :: (a -> [b] -> b) -> [b] -> Tree a -> b
+treeFold1 f init tree = f (rootLabel tree) (map (treeFold1 f init) (subForest tree))
+
+-- Exercise 3
+
+-- We only use the new list if it beats the direct reports
+augmentGL:: Employee -> (GuestList, GuestList) -> (GuestList, GuestList)
+augmentGL emp (w, wo) = (max new w, w)
+                        where new = glCons emp wo
+
+maxFirst :: (GuestList, GuestList) -> (GuestList, GuestList)
+maxFirst (a, b)
+  | b > a     = (b, a)
+  | otherwise = (a, b)
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel emp l = augmentGL emp $ foldr (<>) (mempty, mempty) l
+
+-- Exercise 4
+
+maxFun :: Tree Employee -> GuestList
+maxFun = (uncurry moreFun) . (treeFold1 nextLevel [(GL [] 0, GL [] 0)])
