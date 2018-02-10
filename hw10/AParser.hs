513cfd23c52ecec87cc10b48a39b1a2d17729608
Anubhav Srivastava <misteranubhav@gmail.com>
2018-02-10 11:48:26 -0800

    Finally did my homework. hw10 improved and hw11 completed.

--- hw10/AParser.hs
+++ hw10/AParser.hs
@@ -20,13 +20,13 @@ newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
 -- does not satisfy the predicate (or an empty input), it fails.
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = Parser f
-	where
-	f [] = Nothing		-- fail on the empty input
-	f (x:xs)			-- check if x satisfies the predicate
-						-- if so, return x along with the remainder
-						-- of the input (that is, xs)
-		| p x		= Just (x, xs)
-		| otherwise	= Nothing  -- otherwise, fail
+    where
+    f [] = Nothing      -- fail on the empty input
+    f (x:xs)            -- check if x satisfies the predicate
+                        -- if so, return x along with the remainder
+                        -- of the input (that is, xs)
+        | p x       = Just (x, xs)
+        | otherwise = Nothing  -- otherwise, fail
 
 -- Using satisfy, we can define the parser 'char c' which expects to
 -- see exactly the character c, and fails otherwise.
@@ -49,10 +49,10 @@ Just ('x',"yz")
 posInt :: Parser Integer
 posInt = Parser f
   where
-	f xs
-		| null ns	= Nothing
-		| otherwise	= Just (read ns, rest)
-		where (ns, rest) = span isDigit xs
+    f xs
+        | null ns   = Nothing
+        | otherwise = Just (read ns, rest)
+        where (ns, rest) = span isDigit xs
 
 ------------------------------------------------------------
 -- Your code goes below here
@@ -61,65 +61,38 @@ posInt = Parser f
 first :: (a -> b) -> (a,c) -> (b,c)
 first fn (f,s) = (fn f, s)
 
-safeAp :: (a -> b) -> Maybe (a, String) -> Maybe (b, String)
-safeAp fn Nothing = Nothing
-safeAp fn (Just val) = Just (first fn val)
-
--- (a -> b) -> f a -> f b
+-- (a -> b) -> Parser a -> Parser b
+-- runParser returns Maybe (a,String)
 instance Functor Parser where
-	fmap fn p = Parser {runParser = (safeAp fn) . (runParser p)}
+    fmap fn p = Parser {runParser = fmap (first fn) . (runParser p)}
 
 
-applyToMaybe :: (a -> b) -> Maybe (a, String) -> Maybe (b, String)
-applyToMaybe _ Nothing = Nothing
-applyToMaybe partFunc (Just (val, rest)) = Just ((partFunc val), rest)
+-- Can't be shortened.
+-- fmap will always return something in a maybe context, so if the original 
+-- function is also indeterministic (returns something in a maybe context) we
+-- would have nested maybes.
 
-
-ranFirstApp :: (String -> Maybe (a, String)) -> Maybe ((a -> b), String) -> Maybe (b, String)
-ranFirstApp _ Nothing = Nothing
-ranFirstApp rp2 (Just (partFunc, rest)) = applyToMaybe partFunc (rp2 rest)
+runContinuation :: Parser a -> Maybe (a->b, String) -> Maybe (b, String)
+runContinuation _ Nothing = Nothing
+runContinuation p (Just (k, rest)) = (runParser (fmap k p)) rest
 
 
 instance Applicative Parser where
-	pure fn = Parser {runParser = \s -> Just (fn, s)}
-	(<*>) (Parser rp1) (Parser rp2) = Parser {runParser = (ranFirstApp rp2) . rp1}
-
-
-makePair :: Char -> Char -> (Char, Char)
-makePair a b = (a,b)
+ pure a = Parser {runParser = \input -> Just(a,input)}
+ p1 <*> p2 = Parser { runParser = (runContinuation p2) . (runParser p1)}
 
 abParser :: Parser (Char, Char)
-abParser = makePair <$> (char 'a') <*> (char 'b')
-
-
-burnTwo :: Char -> Char -> ()
-burnTwo _ _ = ()
+abParser = liftA2 (,) (char 'a') (char 'b')
 
 abParser_ :: Parser ()
-abParser_ = burnTwo <$> (char 'a') <*> (char 'b')
-
-
-pairFormat :: Integer -> Char -> Integer -> [Integer]
-pairFormat a _ b = [a, b]
+abParser_ = (\_ _ -> ()) <$> char 'a' <*> char 'b'
 
 intPair :: Parser [Integer]
-intPair = pairFormat <$> posInt <*> (char ' ') <*> posInt
-
-
+intPair = (\first _ second -> [first, second]) <$> posInt <*> char ' ' <*> posInt
 
 instance Alternative Parser where
-	empty = Parser {runParser = \_ -> Nothing}
-	(<|>) (Parser rp1) (Parser rp2) = Parser {runParser = \s -> ((rp1 s) <|> (rp2 s))}
-
-
-
-negIntParser :: Parser ()
-negIntParser = (\_ _ -> ()) <$> (char '-') <*> posInt
-
-
-burn :: Parser a -> Parser ()
-burn p = (\_ -> ()) <$> p
-
+    empty = Parser {runParser = \_ -> Nothing}
+    p1 <|> p2 = Parser {runParser = \input -> ((runParser p1) input) <|> ((runParser p2) input)}
 
 intOrUppercase :: Parser ()
-intOrUppercase = (burn (satisfy isUpper)) <|> negIntParser <|> (burn posInt)
+intOrUppercase = ((\_ -> ()) <$> posInt) <|> ((\_ -> ()) <$> (satisfy isUpper))
